13be8ad77dc8277bfc67ad06fbc11b0cd37a10f8
Dan Fields <daniel.laurent.fields@gmail.com>
2022-09-02 22:57:31 -0400

    introduced patient core component

---
+++ src/components/PatientCore.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { NDivider, NCard, NDescriptions, NDescriptionsItem } from "naive-ui";
+
+import type Patient from "@/models/Patient";
+import PatientTagList from "./PatientTagList.vue";
+import GenderIcon from "./GenderIcon.vue";
+
+interface Props {
+  patient: Patient;
+}
+
+const props = defineProps<Props>();
+const displayName = `${props.patient.last_name}, ${props.patient.first_name}`;
+</script>
+<template>
+  <n-card :title="displayName">
+    <template #header-extra>
+      {{ props.patient.age }}
+      <GenderIcon :gender="props.patient.gender" />
+    </template>
+    <PatientTagList :tags="props.patient.tags" />
+    <template #footer>
+      <slot></slot>
+    </template>
+  </n-card>
+</template>
--- src/components/PatientDemographics.vue
+++ src/components/PatientDemographics.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import {
-  NDivider,
-  NCard,
-  NDescriptions,
-  NDescriptionsItem,
-} from "naive-ui";
+import { NDivider, NCard, NDescriptions, NDescriptionsItem } from "naive-ui";
 
 import type Patient from "@/models/Patient";
 import PatientTagList from "./PatientTagList.vue";
 import GenderIcon from "./GenderIcon.vue";
+import PatientCore from "./PatientCore.vue";
 
 interface Props {
   patient: Patient;
@@ -18,39 +14,32 @@ const props = defineProps<Props>();
 const displayName = `${props.patient.last_name}, ${props.patient.first_name}`;
 </script>
 <template>
-  <n-card :title="displayName">
-    <template #header-extra>
-      {{ props.patient.age }}
-      <GenderIcon :gender="props.patient.gender" />
-    </template>
-    <PatientTagList :tags="props.patient.tags" />
-    <template #footer>
-        <n-divider title-placement="left">demographics</n-divider>
-      <n-descriptions label-placement="top">
-        <n-descriptions-item label="Institution">
-          {{ props.patient.institution }}
-        </n-descriptions-item>
-        <n-descriptions-item label="Medical Record Number">
-          {{ props.patient.mrn }}
-        </n-descriptions-item>
-        <n-descriptions-item label="Date Of Birth">
-          {{ props.patient.dob.toLocaleDateString() }}
-        </n-descriptions-item>
-      </n-descriptions>
-      <n-divider title-placement="left">discharge info</n-divider>
-      <n-descriptions label-placement="top">
-        <n-descriptions-item label="Facility">
-          ...some facility
-        </n-descriptions-item>
-        <n-descriptions-item label="Date / Time">
-          {{
-            `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
-          }}
-        </n-descriptions-item>
-        <n-descriptions-item label="Event">
-          Event Link / Component
-        </n-descriptions-item>
-      </n-descriptions>
-    </template>
-  </n-card>
+  <PatientCore :patient="props.patient">
+    <n-divider title-placement="left">demographics</n-divider>
+    <n-descriptions label-placement="top">
+      <n-descriptions-item label="Institution">
+        {{ props.patient.institution }}
+      </n-descriptions-item>
+      <n-descriptions-item label="Medical Record Number">
+        {{ props.patient.mrn }}
+      </n-descriptions-item>
+      <n-descriptions-item label="Date Of Birth">
+        {{ props.patient.dob.toLocaleDateString() }}
+      </n-descriptions-item>
+    </n-descriptions>
+    <n-divider title-placement="left">discharge info</n-divider>
+    <n-descriptions label-placement="top">
+      <n-descriptions-item label="Facility">
+        ...some facility
+      </n-descriptions-item>
+      <n-descriptions-item label="Date / Time">
+        {{
+          `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
+        }}
+      </n-descriptions-item>
+      <n-descriptions-item label="Event">
+        Event Link / Component
+      </n-descriptions-item>
+    </n-descriptions>
+  </PatientCore>
 </template>
